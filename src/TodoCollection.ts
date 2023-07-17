dee47d2554adf78e7893a457436dcd1f860edbe4
chobolevel <rodaka123@naver.com>
2023-07-17 21:39:07 +0900

    refactor: 할 일 목록 배열이 아닌 Map으로 관리하도록 수정
    todoItem 파라미터 통해 배열로 받아 Map으로 변환하여 관리
    getTodoItems 메서드 추가(true: 모든 할일 목록/false: 완료 제외 목록)
    removeComplete 메서드 추가(완료된 목록 제거)

--- src/TodoCollection.ts
+++ src/TodoCollection.ts
@@ -2,20 +2,39 @@ import TodoItem from "./TodoItem";
 
 class TodoCollection {
     private nextId: number = 1
+    private itemMap: Map<number, TodoItem>
 
-    constructor(public userName: string,
-                public todoItems: TodoItem[] = []) {}
+    constructor(public userName: string, todoItems: TodoItem[] = []) {
+        this.itemMap = new Map<number, TodoItem>()
+        todoItems.forEach(item => this.itemMap.set(item.id, item))
+    }
 
     getTodoById(id: number): TodoItem | undefined {
-        return this.todoItems.find(item => item.id === id)
+        return this.itemMap.get(id)
     }
 
     addTodo(task: string): number {
         while(this.getTodoById(this.nextId)) {
             this.nextId++
         }
-        this.todoItems.push(new TodoItem(this.nextId, task))
+        this.itemMap.set(this.nextId, new TodoItem(this.nextId, task))
         return this.nextId
+    }
+
+    // includeComplete -> true 모든 할 일 반환
+    // includeComplete -> false 한 일은 제외하고 반환
+    getTodoItems(includeComplete: boolean): TodoItem[] {
+        return [...this.itemMap.values()].filter((item) => {
+            return includeComplete || !item.complete
+        })
+    }
+
+    removeComplete(): void {
+        this.itemMap.forEach((item) => {
+            if(item.complete) {
+                this.itemMap.delete(item.id)
+            }
+        })
     }
 
     markComplete(id: number, complete: boolean): void {
